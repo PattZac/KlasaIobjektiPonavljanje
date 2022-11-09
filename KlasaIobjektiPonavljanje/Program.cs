717377d5dbd302c97ad154c9bcf52ce838039838
Pat <113007904+PattZac@users.noreply.github.com>
2022-11-09 08:45:06 +0100

    Add files via upload

---
+++ KlasaIobjektiPonavljanje/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlasaIobjektiPonavljanje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stablo stablo1 = new Stablo(false);
+            Stablo stablo2 = new Stablo(true);
+
+            Console.WriteLine("Stablo 1");
+            Console.WriteLine(stablo1.ToString());
+            Console.WriteLine("\r\nStablo 2");
+            Console.WriteLine(stablo2.ToString());
+            Console.ReadKey();
+        }
+        class Stablo
+        {
+            private bool otpadajuListovi;
+            private string vrstaStabla;
+
+            public bool OtpadajuListovi { get => otpadajuListovi; set => otpadajuListovi = value; }
+            public string VrstaStabla { get => vrstaStabla; set => vrstaStabla = value; }
+
+            public override string ToString()
+            {
+                string otpis = "Vrsta Stabla je " + VrstaStabla + ".";
+                return otpis;
+            }
+            public Stablo(bool otpadajulistovi) 
+            {
+                this.OtpadajuListovi = otpadajulistovi;
+                if (this.OtpadajuListovi)
+                {
+                    this.VrstaStabla = "bijelogorično";
+                }
+                else
+                {
+                    this.VrstaStabla = "crnogorično";
+                }
+            }
+        }
+    }
+}
